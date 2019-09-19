0bd419e878b4a9850b415d7e151c7ab02a1a011f
Jesus <jralvarez1986@gmail.com>
2019-09-19 21:03:25 +0200

    crated functios to add columns of geopoint and raised aomunts per company

--- src/data_processing.py
+++ src/data_processing.py
@@ -30,3 +30,36 @@ def expand_dataframe_by_offices(df):
            'twitter_username', 'updated_at', 'video_embeds'], value_name = "offices") \
         .drop("variable", axis = 1)
     return df
+
+
+def get_lat(x): 
+    # Get latitude for a given office object
+    try: 
+        lat = x.get('latitude')
+    except: 
+        lat = None
+    return lat
+
+def get_lon(x): 
+    # Get longitude for a given office object
+    try: 
+        lat = x.get('longitude')
+    except: 
+        lat = None
+    return lat
+
+def geopoint(office): 
+    # Creates a geopoint object from an give office
+    return {"type": "Point", "coordinates": [get_lon(office),get_lat(office)]}
+
+def add_geopoint_to_df(df):
+    # Adds a geopoints column to a Dataframe.
+    df['geo']= df.offices.apply(geopoint)
+    return df
+
+def get_amouts_raised(funding_round):
+    return [x.get('raised_amount') for x in funding_round]
+
+def add_raised_to_df(df):
+    df['Raised']=df.funding_rounds.apply(get_amouts_raised)
+    return df
