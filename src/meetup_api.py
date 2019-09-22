67f4740f6ed67eb5cea6e04bc9169852c89b8f9e
Jesus Rodriguez <jralvarez1986@gmail.com>
2019-09-22 16:35:21 +0200

    meetup API requests working

--- src/meetup_api.py
+++ src/meetup_api.py
@@ -5,3 +5,9 @@ dotenv.load_dotenv()
 
 
 meetup_token = os.getenv("MEETUP_KEY")
+
+def getAuth(lon,lat, headers={"KEY={}=True".format(meetup_token)}):
+    baseUrl = "https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon={}}&page=20&radius=10&lat={}}".format(lon,lat)
+    print("Asking {}".format(baseUrl))
+    res = requests.get(baseUrl, headers=headers)
+    return res.json()
